b1df50719ed5290c597fcd49e74348882a1a76eb
maru <gunkoubaseball@i.softbank.jp>
2021-03-27 07:44:37 +0900

    aaa

--- src/app/Http/Controllers/PostController.php
+++ src/app/Http/Controllers/PostController.php
@@ -9,62 +9,54 @@
  use App\Post;
 
 class PostController extends Controller{
- // // public function index(Request $request)
- // {
- //   $items = $request->input("items");
- //
- //   $query= User::query();
- //
- //  if(!empty($items)){
- //   $query->where('summary','like','%'.$items.'%');
- //   }
- // }
+
  public function index()
- {
-   return Post::all();
- }
+  {
+    return Post::all();
+  }
 
 
- /*public function index(Request $request)
- {
-   return Post::create($request->all());
- }
-*/
- // public function store(Request $request)
- // {
- //   return Post::create($request->all());
- // }
- public function show($id)
- {
-   return Post::find($id);
- }
- public function update(Request $request,$id)
- {
-    $post = Post::find($id);
-    $post->update($request->all());
-    return $post;
- }
- public function destroy($id)
- {
-   return Post::destory($id);
- }
 
- public function find(Request $request)
+
+ public function find()
  {
     return ['input' =>''];
  }
 
- // public function index()
- // {
- //   $posts = Post::all();
- //   return response()->json(['posts'=>$posts],200);
- // }
- //
- //
- public function store(Request $request)
+
+ public function create(Request $request)
  {
-    $title=$request->title;
-    return response()->json(['posts'=>$title],200);
+   $post = new Post;
+   $post->title = $request->title;
+   $post->content = $request->content;
+   $post->save();
+    // $param = [
+    //   'title'=>$request->title,
+    //   'content'=>$request->content,
+    // ];
+    //   DB::table('posts')->insert($param);
+    return response()->json(['posts'=>$post],200);
  }
+
+ public function delete(Request $request,$id)
+ {
+     $post = Post::findOrFail($id);
+     $post->delete();
+     //return response()->json(['posts'=>$posts],200);
+ }
+
+  public function edit(Request $request)
+ {
+    $post = Post::find($request->id);
+}
+
+public function update(Request $request,$id)
+{
+    $post = post::find($request->id);
+    $post->id=$request->id;
+    $post->title = $request->title;
+    $post->content = $request->content;
+    $post->save();
+}
 }
  ?>
--- src/routes/api.php
+++ src/routes/api.php
@@ -16,51 +16,16 @@ use Laravel\Passport\Passport;
 */
 
 
-/*Route::get('/posts',function(){
-  $post= Post::create(['title'=>'my first post',
-   'slug'=>'my-first-post'
-]);
 
-return $post;
-});
-*/
+
 Route::get('/posts','PostController@index');
 
 //create route
-Route::post('/posts','PostController@store');
+Route::post('/posts','PostController@create');
 
-//update
-//Route::store('/posts', 'PostController@index');
 
-Route::delete('/posts','PostController@index');
+Route::post('/delete/{id}','PostController@delete');
 
-//Route::store('/posts', 'PostController@index');
+Route::get('/edit/{id}','PostController@edit');
 
- Route::middleware('auth:api')->get('oauth/clients', function (Request $request) {
-  return $request->user();
- });
-
- //Route::get('/user/{id}', 'UserController@getData')->middleware('auth:api');
-
- Route::group(["middleware" => "api"], function () {
-    Route::post('/login', 'Auth\LoginController@login');
-    Route::get('/current_admin_user', function () {
-        return Auth::user();
-    });
-    Route::post('/admin', 'Auth\LoginController@login');
-    //追加
-    Route::group(['middleware' => ['auth:api']], function () {
-
-    Route::apiResource('admin_users', 'Api\AdminUserController')->except(['show']);
-        //ここに認証が必要なパスを書いていく
-    });
-});
-
-//get:post 記事投稿
-
-// public function resource(Request $request)
-// {
-//   $users=User::find($id);
-//   //viewにデータを渡す
-//   return view('place.show',['items'=>$places]);
-// }
+Route::post('/update/{id}','PostController@update');
