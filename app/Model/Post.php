4ab9a6c6a740be1368c101a1a942a57084418f60
Maruse1006 <oonnwu9119@yahoo.ne.jp>
2021-12-05 20:10:01 +0900

    aa

--- app/Model/Post.php
+++ app/Model/Post.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\PostControllers;
 
 use Illuminate\Database\Eloquent\Model;
 
--- app/PostReview.php
+++ app/PostReview.php
@@ -1,15 +1,15 @@
 <?php
 
-namespace App;
+namespace App\Http\PostControllers;
 
 use Illuminate\Database\Eloquent\Model;
 
 class PostReview extends Model
 {
-    public function user() { // 👈 追加
+    // public function user() { // 👈 追加
 
-        return $this->belongsTo(\App\User::class, 'user_id', 'id')
-            ->select('id', 'name');
+    //     return $this->belongsTo(\App\User::class, 'user_id', 'id')
+    //         ->select('id', 'name');
 
-    }
+    // }
 }
