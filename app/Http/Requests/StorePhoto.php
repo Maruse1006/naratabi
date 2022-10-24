b7f92ce97e64ddf404f643e252013b7627ea3d4b
Maruse1006 <oonnwu9119@yahoo.ne.jp>
2022-10-24 20:23:05 +0900

    aaa

--- app/Http/Requests/StorePhoto.php
+++ app/Http/Requests/StorePhoto.php
@@ -10,7 +10,7 @@ class StorePhoto extends FormRequest
      * Determine if the user is authorized to make this request.
      *
      * @return bool
-     */
+     */  
     public function authorize()
     {
         return false;
