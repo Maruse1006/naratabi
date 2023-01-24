e90c8068203413d6ccb1ceacca8bbcc024eaad69
maru <gunkoubaseball@i.softbank.jp>
2023-01-25 07:18:32 +0900

    postadd

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -24,12 +24,13 @@ class RegisterController extends Controller
   $user = new User();
   $disk = Storage::disk('s3');
   $image = $request->image; 
-  $path = $disk->putFile('images', $image,'public');
-  $user->path=$path;
+  $path = $disk->putFile('images', $image,'public');  
+  $user->path = Storage::disk('s3')->url($path);
+//   $user->path=$path;
    $user->name = $request->name;
    $user->email= $request->email;
    $user->password =Hash::make($request->password);
-   $user->save();
+   
    $user->save();
    
    
