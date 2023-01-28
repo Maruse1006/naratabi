6d38a88b2d820b2c4af1a0791822e9ad9e42f343
maru <gunkoubaseball@i.softbank.jp>
2023-01-28 10:34:04 +0900

    postadd

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -21,33 +21,19 @@ class PostsTableSeeder extends Seeder
 
      $data = [
         [
-          'id'=>11,
-          'name'=>'日航ホテル奈良',
-          'content'=>'JR奈良駅とデッキでつながったホテル。天然温泉「飛鳥の湯」もあり、【Lohas】をコンセプトした、おもてなしが体験できる。',
-          'content2'=>'',
-          'path'=>'https://naratabi.s3.ap-northeast-1.amazonaws.com/images/nikkohotelnara.jpg',
+          'id'=>13,
+          'name'=>'唐招提寺',
+          'content'=>'唐の高僧鑑真和上が聖武天皇に招かれ、12年もの間、幾度にも及ぶ苦難の末に来日を果たしたのち、天平宝字3(759)年...',
+          'content2'=>'唐の高僧鑑真和上が聖武天皇に招かれ、12年もの間、幾度にも及ぶ苦難の末に来日を果たしたのち、天平宝字3(759)年、新田部親王の旧邸を賜り創建。奈良の大寺のほとんどが勅願による官寺であるのに対し、この寺は鑑真和上発願による私院であることが特徴。境内には金堂、講堂、校倉(2棟)、鼓楼(いずれも国宝)をはじめとする伽藍が立ち並んでいる。これらは、朝廷などの寄進により徐々に整えられていったもので、現在でも創建時の姿を伺い知ることができる。また、天平彫刻の傑作も数多く安置されている。なかでも御影堂（現在は修理中）の鑑真和上像(国宝)は、わが国に現存する最古の肖像彫刻で、晩年をむかえた鑑真和上の深い精神性を感じさせる。開山堂前には、和上像に対面して芭蕉が詠んだ「若葉して御目の雫拭はばや」の句碑が立てられている。わが国に残る最大の天平建築である金堂(国宝)は、平成12年から約10年をかけて本格的な解体修理が行われ(世界遺産)、平成21年11月に終了いたしました。',
+          'path'=>'https://naratabi.s3.ap-northeast-1.amazonaws.com/images/tosho.jpg',
           'path2'=>'',
-          'openning_hour'=>'チェックイン　15:00 チェックアウト　11:00',
+          'openning_hour'=>'8:30～17:00（受付は16:30まで）          ',
           'adress'=>'奈良県奈良市登大路町40',
           'access'=>'近鉄奈良駅から徒歩2分',
           'official_url'=>'https://www.nikkonara.jp/',
           'googlemap'=>'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3280.9353703575534!2d135.8162084795222!3d34.68158059184572!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x600150777bcbed7b%3A0xb26b8abf60e80cb6!2z44Ob44OG44Or5pel6Iiq5aWI6Imv!5e0!3m2!1sja!2sjp!4v1674858904927!5m2!1sja!2sjp',
           'category_id'=>'3',
         ],
-        [
-          'id'=>12,
-          'name'=>'スーパーホテルLOHAS･JR奈良駅',
-          'content'=>'奈良最大規模の本格派シティホテル。JR奈良駅西口に直結し、アクセスも抜群。和食、鉄板焼き、バイキングなどレストランも多彩。',
-          'content2'=>'',
-          'path'=>'https://naratabi.s3.ap-northeast-1.amazonaws.com/images/Lohashotel.jpg',
-          'path2'=>'',
-          'openning_hour'=>'チェックイン　15:00 チェックアウト　11:00',
-          'adress'=>'奈良県奈良市登大路町40',
-          'access'=>'近鉄奈良駅から徒歩2分',
-          'official_url'=>'https://www.nikkonara.jp/',
-          'googlemap'=>'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3280.9353703575534!2d135.8162084795222!3d34.68158059184572!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x600150777bcbed7b%3A0xb26b8abf60e80cb6!2z44Ob44OG44Or5pel6Iiq5aWI6Imv!5e0!3m2!1sja!2sjp!4v1674858904927!5m2!1sja!2sjp',
-          'category_id'=>'3',
-        ]
         ];
         DB::table('posts')->insert($data);
          }
