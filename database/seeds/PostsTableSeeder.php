acdf33acbfd43fd4ea39d82f9ba0291a4774e913
maru <gunkoubaseball@i.softbank.jp>
2023-01-30 23:33:21 +0900

    postadd

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -21,17 +21,17 @@ class PostsTableSeeder extends Seeder
 
      $data = [
         [
-          'id'=>15,
-          'name'=>'みたらい渓谷',
-          'content'=>'エメラルドグリーンに輝く神秘的な淵。大小様々な滝と巨岩を縫い底まで透けて見える清流が流れるみたらい渓谷には、川沿いに遊歩道が整備され、つり橋からは滝を上から眺めることもでき、まさに絶景！そのダイナミックな渓谷美に感動！ 新緑がきらきら輝く春、水しぶきに川サツキが美しい夏、まさに天から降ってくるように山頂から色付き全山紅葉の錦秋(11月上旬)、山水画の冬と、四季折々の景観は、近畿地方随一の美しさとたたえられています。',
-          'content2'=>'エメラルドグリーンに輝く神秘的な淵。大小様々な滝と巨岩を縫い底まで透けて見える清流が流れるみたらい渓谷には、川沿いに',
-          'path'=>'https://naratabi.s3.ap-northeast-1.amazonaws.com/images/mitarai.jpg',
-          'path2'=>'https://naratabi.s3.ap-northeast-1.amazonaws.com/images/mitarai2.jpg',
+          'id'=>16,
+          'name'=>'屯鶴峯（どんづるぼう）',
+          'content'=>'千数百万年前に二上山の火山活動によって火砕流や火山灰などが堆積し、その後の地殻変動によって隆起し、さらに、長い年月の間に侵食されて現在の姿になりました。古くは古墳の石棺材や寺院の基壇などの石材として利用されました。遠くから見ると鶴が屯（たむろ）しているような奇観のため、この名称で呼ばれるようになりました。≪県指定天然記念物≫',
+          'content2'=>'千数百万年前に二上山の火山活動によって火砕流や火山灰などが堆積し、その後の地殻変動によって',
+          'path'=>'https://naratabi.s3.ap-northeast-1.amazonaws.com/images/%E3%81%A9%E3%82%93%E3%81%9A%E3%82%8B.jpg',
+          'path2'=>'https://naratabi.s3.ap-northeast-1.amazonaws.com/images/%E3%81%A9%E3%82%93%E3%81%9A%E3%82%8B%EF%BC%92.jpg',
           'openning_hour'=>'',
-          'adress'=>'〒638-0303 吉野郡天川村北角',
-          'access'=>'近鉄 下市口駅から奈良交通バス洞川温泉行及び中庵住行バスで約1時間「天川川合」下車 徒歩40分',
-          'official_url'=>'https://www.vill.tenkawa.nara.jp/#center',
-          'googlemap'=>'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d26386.444061736547!2d135.85400216508157!3d34.24074724939632!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m',
+          'adress'=>'香芝市穴虫地内',
+          'access'=>'近鉄南大阪線「二上山駅」下車、西へ徒歩約32分（約2.5km）',
+          'official_url'=>'https://www.city.kashiba.lg.jp/soshiki/15/4690.html',
+          'googlemap'=>'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13146.040637676544!2d135.6453495395508!3d34.540636400000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60012926bafa96c7%3A0xae3a6e63b13f7a39!2z5bGv6ba05bOv!5e0!3m2!1sja!2sjp!4v1675086462096!5m2!1sja!2sjp',
           'category_id'=>'2',
         ],
         ];
